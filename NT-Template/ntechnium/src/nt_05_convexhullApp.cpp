f61ba7d87f464ad5f546ff279e8021f5836af6dd
jwarton <jwarton@smu.edu>
2016-08-22 15:12:42 -0500

    Convex Hull Test Application

---
+++ NT-Template/ntechnium/src/nt_05_convexhullApp.cpp
@@ -0,0 +1,42 @@
+#include "nt_05_convexhullApp.h"
+
+void convexApp::init() {
+	///////////////////////////////////////////////////////////////
+	////////////////////////////////////////////// OBJECT POSITIONS
+	/// /////////////////////////////
+	std::vector <ntVec3*> pt_cloud;
+
+	for (int i = 0; i < 15; i++) {
+		Vec3* pt0 = new Vec3(-i * 1, i * 1, -i * 1);
+		pt_cloud.push_back(pt0);
+	}
+	/// /////////////////////////////
+	ntVec3* vA = new ntVec3(-15, -15,  15);
+
+	/// /////////////////////////////
+	ntVec3* vB = new ntVec3( 15,  15,  15);
+	ntVec3* vC = new ntVec3( 15, -15,  15);
+	ntVec3* vD = new ntVec3(-15, -15, -15);
+
+	std::vector <ntVec3*> vs;
+	vs.push_back(vB);
+	vs.push_back(vC);
+	vs.push_back(vD);
+	///////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////// OBJECTS
+	polyhedron = ntConvexHull(&pt_cloud);
+
+	polyhedron.add_point(vA);
+	polyhedron.add_points(&vs);
+}
+
+void convexApp::run(){
+	display();
+}
+
+void convexApp::display(){
+	///////////////////////////////////////////////////////////////
+	polyhedron.display();
+
+	///////////////////////////////////////////////////////////////
+}
